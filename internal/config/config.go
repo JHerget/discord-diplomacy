900f1bcea5cf61205578cdd83c56bd2070d6b04a
Jordan Herget <lethalbacon16@gmail.com>
2026-08-19 09:16:05 -0600

    attempt reading state file to load active game

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
@@ -8,14 +9,16 @@ import (
 )
 
 type Config struct {
-	BotToken string
-	GuildID  string
+	BotToken   string
+	GuildID    string
+	ActiveGame *string
 }
 
 func Load() (Config, error) {
 	cfg := Config{
 		BotToken: strings.TrimSpace(os.Getenv("DISCORD_BOT_TOKEN")),
 		GuildID:  strings.TrimSpace(os.Getenv("DISCORD_GUILD_ID")),
+		ActiveGame: nil,
 	}
 
 	var validationErrors []error
@@ -25,9 +28,25 @@ func Load() (Config, error) {
 	if cfg.GuildID == "" {
 		validationErrors = append(validationErrors, fmt.Errorf("DISCORD_GUILD_ID is required"))
 	}
-
 	if err := errors.Join(validationErrors...); err != nil {
 		return Config{}, err
+	}
+
+	data, err := os.ReadFile("/var/lib/discord-diplomacy.json")
+	if err != nil && !errors.Is(err, os.ErrNotExist) {
+		return Config{}, err
+	}
+
+	if len(data) > 0 {
+		var activeGames map[string]string
+		if err := json.Unmarshal(data, &activeGames); err != nil {
+			return Config{}, err
+		}
+
+		gameId, ok := activeGames[cfg.GuildID]
+		if ok {
+			cfg.ActiveGame = &gameId
+		}
 	}
 
 	return cfg, nil
